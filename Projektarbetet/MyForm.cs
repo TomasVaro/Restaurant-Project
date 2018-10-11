18e3c37c50065936c6221273c894c94db9a8d440
Tomas Varo <tomas.varo63@gmail.com>
2018-10-11 23:44:22 +0200

    Version 1 av Projektarbetet

--- Projektarbetet/MyForm.cs
+++ Projektarbetet/MyForm.cs
@@ -13,8 +13,49 @@ namespace Projektarbetet
     {
         public MyForm()
         {
-            Console.WriteLine("Hello Novak");
-            Console.WriteLine("Hello Tomas");
+            Size = new Size(700, 500);      //anger storleken på fönstret
+            {
+
+                TableLayoutPanel table = new TableLayoutPanel
+                {
+                    RowCount = 8,
+                    ColumnCount = 3,
+                    Dock = DockStyle.Fill,
+                    CellBorderStyle = TableLayoutPanelCellBorderStyle.Outset
+                };
+                Controls.Add(table);
+
+                table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33));
+                table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33));
+                table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33));
+                table.RowStyles.Add(new RowStyle(SizeType.Percent, 12));
+                table.RowStyles.Add(new RowStyle(SizeType.Percent, 13));
+                table.RowStyles.Add(new RowStyle(SizeType.Percent, 12));
+                table.RowStyles.Add(new RowStyle(SizeType.Percent, 13));
+                table.RowStyles.Add(new RowStyle(SizeType.Percent, 12));
+                table.RowStyles.Add(new RowStyle(SizeType.Percent, 13));
+                table.RowStyles.Add(new RowStyle(SizeType.Percent, 12));
+                table.RowStyles.Add(new RowStyle(SizeType.Percent, 13));
+
+                Label restaurantName = new Label
+                {
+                    Text = "Restaurang SeeCharp",
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    Dock = DockStyle.Fill,
+                    Font = new Font("Brush Script Std", 30),
+                    AutoSize = true
+                };
+                table.Controls.Add(restaurantName);
+                table.SetColumnSpan(restaurantName, 3);
+
+                ComboBox starter = new ComboBox
+                {                    
+                    DropDownStyle = ComboBoxStyle.DropDownList,
+                    Dock = DockStyle.Fill,
+                };
+                table.Controls.Add(starter);
+                //starter.SelectedIndexChanged += ComboboxChanged;
+            }            
         }
     }
 }
