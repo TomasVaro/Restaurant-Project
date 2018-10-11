6d2fcc0277110944966475a4d24491b9f8cd8440
Tomas Varo <tomas.varo63@gmail.com>
2018-10-11 16:07:34 +0200

    Version 2

---
+++ Projektarbetet/MyForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+
+namespace Projektarbetet
+{
+    class MyForm : Form
+    {
+        public MyForm()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
